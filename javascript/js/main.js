f07971a7218ff44eae7141a30c67312cbafde001
kallyas <kallyasmedia@gmail.com>
2022-09-01 14:12:13 +0300

    ch: add arrays

--- javascript/js/main.js
+++ javascript/js/main.js
@@ -136,3 +136,79 @@ check.addEventListener("click", function () {
         alert("You are not eligible to vote");
     }
 })
+
+
+function checkDay(day = "monday") {
+    if(day === "monday") {
+        console.log("Today is Monday");
+    } else {
+        console.log("Today is not Monday");
+    }
+}
+checkDay()
+checkDay("Tuesday");
+
+// Arrays []
+//              0         1         2      3
+let fruits = ["Apple", "Orange", "Banana", "Mango"];
+
+// indexing arrays
+console.log(fruits)
+console.log(fruits[0])
+console.log(fruits[3])
+console.log(fruits[2])
+console.log(fruits[4])
+
+// print all the elements in the array
+for(let item in fruits) {
+    console.log(fruits[item])
+}
+
+for(let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i])
+}
+
+let i = 0;
+while(i < fruits.length) {
+    console.log(fruits[i])
+    i++;
+}
+
+
+//  Array Methods
+// 1. push() - add an element to the end of the array
+let fruit = prompt("Enter a fruit");
+fruits.push(fruit);
+console.log(fruits);
+
+let number = prompt("How many fruits do you want to add?");
+for(let i = 0; i < number; i++) {
+    let fruit = prompt(`Enter fruit ${i + 1}: `);
+    fruits.push(fruit);
+}
+console.log(fruits);
+
+// 2. pop() - remove an element from the end of the array
+console.log(fruits.pop());
+console.log(fruits);
+
+
+// 3. unshift() - add an element to the beginning of the array
+fruits.unshift("Pineapple")
+console.log(fruits);
+
+// 4. shift() - remove an element from the beginning of the array
+fruits.shift();
+console.log(fruits);
+
+// 5. splice() - remove an element from a specific position in the array
+fruits.splice(2, 1);
+console.log(fruits);
+
+
+// create an array of movies and do the following
+// 1. add a movie to the end of the array
+// 2. remove a movie from the end of the array
+// 3. add a movie to the beginning of the array
+// 4. remove a movie from the beginning of the array
+// 5. remove a movie from a specific position in the array
